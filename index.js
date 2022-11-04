138d3165f304f915ff110fc9ef7b82a138bcf98f
nawalkanabee <nawalkanabee@gmail.com>
2022-11-04 16:38:00 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+ 
+ 
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+    speak() {
+      if (this.sex == "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+ 
+ 
